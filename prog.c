52953130e2fa22f4d5f7f29a2e2f9189ea5fcfcb
Yanistd9 <yanis060918@gmail.com>
2025-10-16 09:28:54 +0200

    Update prog.c
    ajout de cursus

--- prog.c
+++ prog.c
@@ -6,7 +6,8 @@
 
 enum {
     MAX_CHAR = 30,
-    MAX_ETU = 180
+    MAX_ETU = 180,
+    NB_UE = 6
 };
 
 typedef struct {
@@ -22,9 +23,9 @@ void GererErreur() {
 
 void inscription(ETUDIANT tab_etu[], int* nb_etu) {
     char prenom[MAX_CHAR], nom[MAX_CHAR];
-    scanf("%s %s", prenom, nom); // lecture du prénom et nom sur la même ligne
+    scanf("%s %s", prenom, nom); // lecture du prÃ©nom et nom sur la mÃªme ligne
 
-    // Vérification des doublons (même prénom et même nom)
+    // VÃ©rification des doublons (mÃªme prÃ©nom et mÃªme nom)
     for (int i = 0; i < *nb_etu; i++) {
         if (strcmp(tab_etu[i].prenom, prenom) == 0 &&
             strcmp(tab_etu[i].nom, nom) == 0) {
@@ -33,9 +34,9 @@ void inscription(ETUDIANT tab_etu[], int* nb_etu) {
         }
     }
 
-    // Enregistrement du nouvel étudiant
+    // Enregistrement du nouvel Ã©tudiant
     if (*nb_etu < MAX_ETU) {
-        tab_etu[*nb_etu].id = *nb_etu + 1; // commence à 1
+        tab_etu[*nb_etu].id = *nb_etu + 1; // commence Ã  1
         strcpy(tab_etu[*nb_etu].prenom, prenom);
         strcpy(tab_etu[*nb_etu].nom, nom);
         printf("Inscription enregistree (%d)\n", tab_etu[*nb_etu].id);
@@ -45,6 +46,30 @@ void inscription(ETUDIANT tab_etu[], int* nb_etu) {
         printf("Nombre maximum d'etudiants atteint\n");
     }
 }
+// fonction cursus
+void cursus(ETUDIANT tab_etu[], int nb_etu) {
+    int id;
+    scanf("%d", &id); // lecture de lâ€™identifiant
+
+    int trouve = 0;
+    for (int i = 0; i < nb_etu; i++) {
+        if (tab_etu[i].id == id) {
+            trouve = 1;
+            printf("%d %s %s\n", tab_etu[i].id, tab_etu[i].prenom, tab_etu[i].nom);
+            printf("S1 - ");
+            for (int j = 0; j < NB_UE; j++) {
+                printf("* (*)");
+                if (j < NB_UE - 1)
+                    printf(" - ");
+            }
+            printf(" - en cours\n");
+            break;
+        }
+    }
+
+    if (!trouve)
+        printf("Identifiant incorrect\n");
+}
 
 int main() {
     ETUDIANT tab_etu[MAX_ETU];
@@ -52,7 +77,7 @@ int main() {
     int nb_der = 0;
     int nb_etu = 0;
 
-    while (nb_etu < MAX_ETU) {
+    while (1) {
         scanf("%s", saisie);
 
         if (strcmp(saisie, "EXIT") == 0) { // EXIT
@@ -60,6 +85,9 @@ int main() {
         }
         else if (strcmp(saisie, "INSCRIRE") == 0) { // INSCRIRE
             inscription(tab_etu, &nb_etu);
+        }
+        else if (strcmp(saisie, "CURSUS") == 0) {
+            cursus(tab_etu, nb_etu);
         }
         else {
             printf("Commande inconnue\n");
